051214d540ebe48d0433a52b65ce21803f786b85
dakshbajpai419-ux <dakshbajpai419@gmail.com>
2025-08-23 15:22:45 +0530

    Create name address.java

---
+++ name address.java
@@ -0,0 +1,8 @@
+
+class main{
+    public static void main(String[] args){
+        System.out.println("My name is Daksh Bajpai.");
+        System.out.println("Address:- Greater Noida");
+        System.out.println("Pin:- 255018");
+    }
+}
